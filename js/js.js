3597b3f9cd52102db248792f39f8b0ba42d18b77
Luiz Neto <luizgonzaga15@gmail.com>
2021-12-11 20:28:51 -0300

    colocando o fundo para movimentar

--- js/js.js
+++ js/js.js
@@ -7,5 +7,18 @@ function start() //inicio da função start
     $('#fundoGame').append('<div id="inimigo2"> </div>')
     $('#fundoGame').append('<div id="amigo" class="anima3"> </div>')
 
+    var jogo = {}
+
+    jogo.timer = setInterval(loop,30)
+
+    function loop(){
+        moveFundo()
+    }
+
+    function moveFundo(){
+        esquerda = parseInt($('#fundoGame').css('background-position'))
+        $('#fundoGame').css('background-position',esquerda-1)     
+
+    }
 
 } // Fim da função start
